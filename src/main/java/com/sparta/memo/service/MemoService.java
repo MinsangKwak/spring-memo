dd3c20b75da0da49914cb0dcd638d8d92ec39a5a
minsangKwak <kmsdevwork@gmail.com>
2024-01-07 22:30:10 +0900

    [update] service, repository annotation으로 목적에 맞게 변경

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -6,15 +6,27 @@ import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Component
+@Service
 public class MemoService {
     private final MemoRepository memoRepository;
     public MemoService(MemoRepository memoRepository) {
         this.memoRepository = memoRepository;
     }
+
+    // public MemoService(ApplicationContext context) {
+        // 1. Bean 이름으로 가져오기
+        // MemoRepository memoRepository = (MemoRepository) context.getBean("memoRepository");
+
+        // 2. Bean 클래스 형식으로 가져오기
+        // MemoRepository memoRepository = context.getBean(MemoRepository.class);
+        // this.memoRepository = memoRepository;
+    // }
+
+
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
         // RequestDto -> Entity
         Memo memo = new Memo(requestDto);
